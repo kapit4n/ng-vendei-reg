b384f0d0b174a5a61468f705547fc9f5d41f8dff
lartl <luis.arce22@gmail.com>
2018-10-27 10:34:10 -0400

    Adding router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 
@@ -8,9 +9,22 @@ import { RCategoryService } from "./services/reg/r-category.service";
 import { RegProductComponent } from './pages/reg/reg-product/reg-product.component';
 import { RegCategoryComponent } from './pages/reg/reg-category/reg-category.component';
 
+
+const appRoutes: Routes = [
+  {
+    path: "home",
+    component: RegProductComponent,
+    data: { breadcrumb: "Register Product" }
+  }
+];
+
 @NgModule({
   declarations: [AppComponent, RegProductComponent, RegCategoryComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
+  ],
   providers: [RProductService, RCategoryService],
   bootstrap: [AppComponent]
 })
